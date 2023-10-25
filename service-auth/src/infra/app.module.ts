7403133bad2d27ab225824ae1fb0116feb366d64
Matheus Sartori <ms.sartori@outlook.com.br>
2023-10-25 18:38:03 -0300

    feat: infra dependencies

--- service-auth/src/infra/app.module.ts
+++ service-auth/src/infra/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
+import { AuthModule } from './auth/auth.module'
+import { EnvModule } from './env/env.module'
+import { HttpModule } from './http/http.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+    EnvModule,
+  ],
 })
 export class AppModule {}
