c1af5c646963737d59cc835c8cc6bfd86130a11f
Matheus Sartori <ms.sartori@outlook.com.br>
2023-10-26 10:37:09 -0300

    feat: service catalog

---
+++ service-catalog/vitest.config.e2e.ts
@@ -0,0 +1,26 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    alias: {
+      '@': './src',
+      '@test': './test',
+    },
+    root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
