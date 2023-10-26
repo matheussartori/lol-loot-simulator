12fb4a76ed896ae95cd6dbb8e79f07032f79bf9d
Matheus Sartori <ms.sartori@outlook.com.br>
2023-10-26 09:24:45 -0300

    feat: messaging

---
+++ service-loot/src/infra/messaging/kafka.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { ClientKafka } from '@nestjs/microservices'
+import { EnvService } from '../env/env.service'
+
+@Injectable()
+export class KafkaService
+  extends ClientKafka
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(config: EnvService) {
+    super({
+      client: {
+        clientId: 'loot',
+        brokers: [config.get('KAFKA_BROKERS')],
+      },
+    })
+  }
+
+  async onModuleInit() {
+    await this.connect()
+  }
+
+  async onModuleDestroy() {
+    await this.close()
+  }
+}
