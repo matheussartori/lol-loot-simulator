774521ef9ff50d070fe204eb7ee12ddb4f1a3c24
Matheus Sartori <ms.sartori@outlook.com.br>
2023-11-06 12:09:09 -0300

    refactor(loot): database schema

--- service-loot/src/infra/database/prisma/migrations/20231106150854_create_initial_tables/migration.sql
+++ service-loot/src/infra/database/prisma/migrations/20231106150854_create_initial_tables/migration.sql
@@ -37,6 +37,7 @@ CREATE TABLE "user_capsules" (
     "user_id" TEXT NOT NULL,
     "capsule_id" TEXT NOT NULL,
     "opened_at" TIMESTAMP(3),
+    "reward_set_id" TEXT,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "user_capsules_pkey" PRIMARY KEY ("id")
@@ -48,6 +49,7 @@ CREATE TABLE "items" (
     "item_id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "type" "ItemType" NOT NULL,
+    "rarity_tier" "RarityTier" NOT NULL,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
@@ -59,11 +61,28 @@ CREATE TABLE "capsule_items" (
     "id" TEXT NOT NULL,
     "capsule_id" TEXT NOT NULL,
     "item_id" TEXT NOT NULL,
-    "rarity_tier" "RarityTier" NOT NULL,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "capsule_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reward_set" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "reward_set_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rewards" (
+    "id" TEXT NOT NULL,
+    "reward_set_id" TEXT NOT NULL,
+    "item_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "rewards_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -85,7 +104,13 @@ CREATE UNIQUE INDEX "items_item_id_key" ON "items"("item_id");
 CREATE INDEX "items_type_idx" ON "items"("type");
 
 -- AddForeignKey
+ALTER TABLE "user_capsules" ADD CONSTRAINT "user_capsules_reward_set_id_fkey" FOREIGN KEY ("reward_set_id") REFERENCES "reward_set"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "capsule_items" ADD CONSTRAINT "capsule_items_capsule_id_fkey" FOREIGN KEY ("capsule_id") REFERENCES "capsules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "capsule_items" ADD CONSTRAINT "capsule_items_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "items"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rewards" ADD CONSTRAINT "rewards_reward_set_id_fkey" FOREIGN KEY ("reward_set_id") REFERENCES "reward_set"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
