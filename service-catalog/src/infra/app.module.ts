4ae5a4c7a3924fb8121ab5befa337944d8bb4598
Matheus Sartori <ms.sartori@outlook.com.br>
2023-11-09 11:21:04 -0300

    feat(catalog): static images

--- service-catalog/src/infra/app.module.ts
+++ service-catalog/src/infra/app.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config'
 import { EnvModule } from './env/env.module'
 import { envSchema } from './env/env'
 import { MessagingModule } from './messaging/messaging.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'static'),
+      serveRoot: '/static',
+    }),
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
