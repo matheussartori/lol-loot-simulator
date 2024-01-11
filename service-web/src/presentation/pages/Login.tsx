43fec88824731229560e19ff91b6f36fd737a88c
Matheus Sartori <ms.sartori@outlook.com.br>
2024-01-11 17:05:16 -0300

    feat: login using react hook form

--- service-web/src/presentation/pages/Login.tsx
+++ service-web/src/presentation/pages/Login.tsx
@@ -4,24 +4,26 @@ import { Button } from '@/presentation/components/Button'
 import { ArrowRight } from '@phosphor-icons/react/dist/ssr'
 import Link from 'next/link'
 import { type Authentication } from '@/domain/use-cases/authentication'
-import { useState, type FormEvent } from 'react'
 import { useAppDispatch } from '../data/store'
 import { setAccessToken } from '../data/slices/userSlice'
 import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
 
 interface LoginProps {
   authentication: Authentication
 }
 
+interface LoginFormData {
+  username: string
+  password: string
+}
+
 export function Login ({ authentication }: LoginProps): JSX.Element {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>()
   const dispatch = useAppDispatch()
   const router = useRouter()
 
-  async function handleLoginSubmit (event: FormEvent) {
-    event.preventDefault()
-
+  async function handleLoginSubmit ({ username, password }: LoginFormData) {
     try {
       const response = await authentication.auth({
         username,
@@ -40,10 +42,12 @@ export function Login ({ authentication }: LoginProps): JSX.Element {
     <main className="flex items-stretch h-[100vh]">
       <div className="flex flex-col items-center justify-center w-[100%] max-w-[500px]">
 
-        <form onSubmit={handleLoginSubmit}>
+        <form onSubmit={handleSubmit(handleLoginSubmit)}>
         <Text className="font-bold text-xl text-center mb-8">Sign in</Text>
-          <Input id="username" placeholder="Username" autoComplete="off" onChange={e => setUsername(e.target.value)} />
-          <Input id="password" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+          <Input id="username" placeholder="Username" autoComplete="off" {...register('username', { required: true })} />
+          {errors.username && <Text className="text-red-500">{errors.username.message}</Text>}
+          <Input id="password" type="password" placeholder="Password" {...register('password', { required: true })} />
+          {errors.password && <Text className="text-red-500">{errors.password.message}</Text>}
           <div className="flex w-[100%] justify-center">
             <Button className="bg-red-600 p-4 rounded-xl">
               <ArrowRight className="text-white" size={28} />
